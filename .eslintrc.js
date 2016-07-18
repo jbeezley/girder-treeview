b4761d8c1aae234ccd3b5d3c8b826aab36071711
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-07-18 14:57:57 -0400

    Checkpoint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "extends": "eslint:recommended",
-    "globals": {
-        "$": true
+    "env": {
+        "browser": false,
+        "node": true
     }
 };
--- index.js
+++ index.js
@@ -1,9 +1,88 @@
+/**
+ * @param {object} [options]
+ * @param {string} [api=girder.apiRoot]
+ *   URL to a girder instance
+ * @param {string} [token=girder.currentToken]
+ *   An authentication token to pass with rest requests.
+ * @param {object} [glyph]
+ *   A mapping of icon classes as described by
+ *   https://github.com/mar10/fancytree/wiki/ExtGlyph
+ * @param {boolean} [persist=false]
+ *   Persist the tree in local storage.  WARNING: because
+ *   nodes are loaded lazily, this will flood the server
+ *   with requests on load.
+ * @param {boolean} [edit=false]
+ *   Allow nodes to be edited and created by the user.
+ * @param {boolean} [dragAndDrop=false]
+ *   Enable drag and drop interactions.
+ * @param {function} [icon]
+ *   A function that should return an icon class.  Takes the model object as an argument.
+ *   Return a falsey value to use the default.
+ * @param {object} [iconMap]
+ *   A mapping of model type to icon class to override defaults.
+ */
+
 $.fn.girderTreeview = function (options) {
+  'use strict';
+
   options = options || {};
-  var api = options.api || 'https://data.kitware.com/api/v1';
+  var girder = window.girder || {};
+
+  var api = options.api || girder.apiRoot;
+  options.icon = options.icon || $.noop;
+
+  var pageSize = options.pageSize || 25;
+
+  var iconMap = $.extend({
+    collection: 'icon-database',
+    user: 'icon-user',
+    item: 'icon-docs',
+    folder: 'icon-folder',
+    file: 'icon-doc-text',
+    home: 'icon-home',
+    users: 'icon-users',
+    collections: 'icon-sitemap',
+    image: 'icon-file-image',
+    javascript: 'icon-file-code',
+    xml: 'icon-file-code',
+    pdf: 'icon-file-pdf'
+      
+  }, options.iconMap);
+
+  var lock = false;
+  var icon = function (evt, data) {
+    var model = data.node.data.model || {};
+    var modelType = data.node.data.root || model._modelType;
+
+    if (modelType === 'file') {
+      switch (model.mimeType) {
+        case 'application/json':
+        case 'text/javascript':
+          modelType = 'javascript';
+          break;
+        case 'application/xml':
+        case 'text/xml':
+        case 'text/html':
+          modelType = 'xml';
+          break;
+        case 'image/jpeg':
+        case 'image/png':
+          modelType = 'image';
+          break;
+        case 'application/pdf':
+          modelType = 'pdf';
+          break;
+      }
+    }
+    return options.icon(model) || iconMap[modelType];
+  };
+
+  function writeable(event, data) {
+    return !lock && !!data.node.data.write;
+  }
 
   var glyph_opts = {
-    map: {
+    map: $.extend({
       doc: "glyphicon glyphicon-file",
       docOpen: "glyphicon glyphicon-file",
       checkbox: "glyphicon glyphicon-unchecked",
@@ -18,7 +97,101 @@ $.fn.girderTreeview = function (options) {
       folder: "glyphicon glyphicon-folder-close",
       folderOpen: "glyphicon glyphicon-folder-open",
       loading: "glyphicon glyphicon-refresh glyphicon-spin"
+    }, options.glyph)
+  };
+
+  var dnd_opts = {
+    autoExpandMS: 500,
+    draggable: {
+      revert: 'invalid'
+    },
+    dragStart: function (node, data) {
+      return data.node.data.model._modelType in move &&
+        writeable(node, data);
+    },
+    dragEnter: function (node, data) {
+      return writeable(null, data) &&
+        node.data.parentOf.indexOf(data.otherNode.data.model._modelType) >= 0 &&
+        'over';
+    },
+    dragDrop: function (node, data) {
+      lock = true;
+      var model = data.otherNode.data.model;
+      var newParent = data.node.data.model;
+      var oldParent = data.otherNode.data.parent.model;
+      move[model._modelType](model, oldParent, newParent)
+        .then(function () {
+          data.otherNode.moveTo(node, data.hitMode);
+          lock = false;
+          return data.otherNode.makeVisible();
+        }, function () {
+          console.error('Move failed on ' + model._id); // eslint-disable-line no-console
+          lock = false;
+        });
     }
+  };
+
+  var persist_opts = {
+    expandLazy: true,
+    store: 'local'
+  };
+
+  var edit_opts = {
+    adjustWidthOfs: 10,
+    beforeEdit: writeable,
+    save: function (event, data) {
+      var newName = data.input.val();
+      var oldName = data.node.data.model.name;
+
+      lock = true;
+      return rename(data.node.data.model, newName)
+        .then(function () {
+          data.node.data.model.name = newName;
+          $(data.node.span).removeClass('pending'); // TODO add css to handle this
+          lock = false;
+        }, function () {
+          data.node.setTitle(oldName);
+          $(data.node.span).removeClass('pending');
+          lock = false;
+        });
+    },
+    close: function (event, data) {
+      if (data.save) {
+        $(data.node.span).addClass('pending');
+      }
+    }
+  };
+
+  var move = {
+    folder: function (model, oldParent, newParent) {
+      return restRequest({
+        url: api + '/folder/' + model._id,
+        method: 'PUT',
+        data: {
+          parentId: newParent._id,
+          parentType: newParent._modelType
+        }
+      });
+    },
+    item: function (model, oldParent, newParent) {
+      return restRequest({
+        url: api + '/item/' + model._id,
+        method: 'PUT',
+        data: {
+          folderId: newParent._id
+        }
+      });
+    }
+  };
+
+  var rename = function (model, name) {
+    return restRequest({
+      url: api + '/' + model._modelType + '/' + model._id,
+      method: 'PUT',
+      data: {
+        name: name
+      }
+    });
   };
 
   var process = {
@@ -27,6 +200,7 @@ $.fn.girderTreeview = function (options) {
         title: model.name,
         folder: true,
         key: model._id,
+        write: model._accessLevel >= 1,
         lazy: true,
         rest: [{
           url: api + '/folder',
@@ -34,44 +208,58 @@ $.fn.girderTreeview = function (options) {
             parentType: 'collection',
             parentId: model._id
           }
-        }]
+        }],
+        model: model,
+        parentOf: ['folder']
       };
     },
-    folder: function (model) {
+    folder: function (model, parent) {
       return {
         title: model.name,
         folder: true,
         key: model._id,
+        write: model._accessLevel >= 1,
         lazy: true,
         rest: [{
+          url: api + '/item',
+          data: {
+            folderId: model._id
+          }
+        }, {
           url: api + '/folder',
           data: {
             parentType: 'folder',
             parentId: model._id
           }
-        }, {
-          url: api + '/item',
-          data: {
-            folderId: model._id
-          }
-        }]
+        }],
+        model: model,
+        parent: parent,
+        parentOf: ['folder', 'item']
       };
     },
-    item: function (model) {
+    item: function (model, parent) {
       return {
         title: model.name,
         folder: true,
         key: model._id,
+        write: parent.write,
         lazy: true,
         rest: [{
           url: api + '/item/' + model._id + '/files'
-        }]
+        }],
+        model: model,
+        parent: parent,
+        parentOf: ['file']
       };
     },
-    file: function (model) {
+    file: function (model, parent) {
       return {
         title: model.name,
-        key: model._id
+        write: parent.write,
+        key: model._id,
+        model: model,
+        parent: parent,
+        parentOf: []
       };
     },
     user: function (model) {
@@ -79,6 +267,7 @@ $.fn.girderTreeview = function (options) {
         title: model.login,
         folder: true,
         key: model._id,
+        write: model._accessLevel >= 1,
         lazy: true,
         rest: [{
           url: api + '/folder',
@@ -86,45 +275,160 @@ $.fn.girderTreeview = function (options) {
             parentType: 'user',
             parentId: model._id
           }
-        }]
+        }],
+        model: model,
+        parentOf: ['folder']
       };
+    },
+    _more: function (model) {
+      return model;
     }
   };
 
-  function postProcess(data) {
+  function postProcess(data, parent) {
     return data.map(function (model) {
-        return process[model._modelType](model);
+        return process[model._modelType](model, parent);
     });
+  }
+
+  function nextPage(rest) {
+    var params = $.extend({offset: 0, limit: pageSize}, rest.data);
+    params.offset += pageSize;
+    return $.extend({}, rest, {data: params});
+  }
+
+  function clickPaging(evt, data) {
+    lazyLoad(evt, data);
+    data.node.replaceWith(data.result);
+  }
+
+  function restRequest(rest) {
+    var token = options.token || girder.currentToken;
+
+    rest.data = $.extend({}, rest.data || {});
+
+    if (token) {
+      $.extend(true, rest, {
+        headers: {
+          'Girder-Token': token
+        }
+      });
+    }
+
+    return $.ajax(rest);
   }
 
   function lazyLoad(event, data) {
     var children = [];
     var promises = [];
+    var parent = data.node.data.parent;
 
     promises = data.node.data.rest.map(function (rest) {
-      return $.ajax(rest)
+
+      rest.data = $.extend(rest.data || {}, {limit: pageSize, offset: 0}, rest.data);
+      return restRequest(rest)
         .then(function (data) {
-            Array.prototype.push.apply(children, data);
+          Array.prototype.push.apply(children, data);
+          if (data.length >= pageSize) {
+            children.push({
+              title: 'Load more...',
+              statusNodeType: 'paging',
+              icon: false,
+              rest: [nextPage(rest)],
+              _modelType: '_more',
+              parentOf: [],
+              model: {},
+              parent: parent
+            });
+          }
         });
     });
     data.result = $.when.apply($, promises).then(function () {
-      return postProcess(children);
+      return postProcess(children, data.node.data);
     });
   }
+
+  var extensions = ['glyph'];
+  if (options.dragAndDrop) {
+    extensions.push('dnd');
+  }
+  if (options.edit) {
+    extensions.push('edit');
+  }
+  if (options.persist) {
+    extensions.push('persist');
+  }
+
+  var source = $.ajax({
+    url: api + '/user/me',
+    headers: {
+      'Girder-Token': options.token || girder.currentToken
+    }
+  }).then(function (data) {
+    var sources = [];
+    var home;
+
+    if (data) {
+      home = $.extend(
+        process.user(data),
+        {
+          title: 'Home',
+          root: 'home',
+          parentOf: []
+        }
+      );
+      sources.push(home);
+    }
+    sources.push({
+      title: 'Collections',
+      key: '2',
+      folder: true,
+      lazy: true,
+      rest: [{
+        url: api + '/collection'
+      }],
+      root: 'collections',
+      model: {},
+      parentOf: []
+    });
+
+    sources.push({
+      title: 'Users',
+      key: '3',
+      folder: true,
+      lazy: true,
+      rest: [{
+        url: api + '/user',
+        data: {
+          sort: 'login'
+        }
+      }],
+      root: 'users',
+      model: {},
+      parentOf: []
+    });
+
+    return sources;
+  });
 
   return this.each(function () {
     var $el = $(this);
 
     $el.fancytree({
-      extensions: ['glyph'],
-      source: [
-        // {title: 'Home', key: '1', folder: true, lazy: true, url: api + '/user/me'},
-        {title: 'Collections', key: '2', folder: true, lazy: true, rest: [{url: api + '/collection'}]},
-        {title: 'Users', key: '3', folder: true, lazy: true, rest: [{url: api + '/user', data: {sort: 'login'}}]}
-      ],
+      extensions: extensions,
+      source: source,
       lazyLoad: lazyLoad,
       postProcess: postProcess,
-      glyph: glyph_opts
+      glyph: glyph_opts,
+      dnd: dnd_opts,
+      persist: persist_opts,
+      edit: edit_opts,
+      focus: function (evt, data) {
+        $el.trigger('g-focus', data.node);
+        window.node = data.node;
+      },
+      icon: icon,
+      clickPaging: clickPaging
     });
   });
 };
